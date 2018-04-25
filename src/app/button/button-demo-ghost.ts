4d7bd38c3bdf3f286b9e824c92b30ab4e0c4e2b6
Trotyl <trotyl@qq.com>
2018-04-26 00:03:55 +0800

    docs: optimize demo display

--- src/app/button/button-demo-ghost.ts
+++ src/app/button/button-demo-ghost.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'button-demo-ghost',
   templateUrl: './button-demo-ghost.html',
+  styleUrls: [`./button-demo-ghost.css`],
   host: { '[style.display]': `'block'` },
 })
 export class ButtonDemoGhost { }
