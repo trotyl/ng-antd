1a34ed634b1db8c36d27904f055e2023c5b31b3a
Trotyl <trotyl@qq.com>
2018-04-25 20:31:54 +0800

    build: upgrade dep version

--- src/app/lib-proxy.ts
+++
@@ -1,7 +0,0 @@
-export * from '../lib/button/button.module'
-export * from '../lib/dropdown/dropdown.module'
-export * from '../lib/grid/grid.module'
-export * from '../lib/icon/icon.module'
-export * from '../lib/layout/layout.module'
-export * from '../lib/menu/menu.module'
-export * from '../lib/radio/radio.module'
