cdaec2c323b278f319315b457f3a74d719133744
Trotyl <trotyl@qq.com>
2018-04-25 22:57:14 +0800

    docs: enable code highlight

---
+++ src/app/shared/highlight.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, Attribute, ElementRef, Renderer2, AfterViewInit } from '@angular/core'
+
+declare var Prism: {
+  highlightElement(el: any): void
+}
+
+@Directive({
+  selector: '[highlight]'
+})
+export class HightLight implements AfterViewInit {
+  constructor(
+    private element: ElementRef,
+    renderer: Renderer2,
+    @Attribute('highlight') lang: string,
+  ) {
+    renderer.addClass(element.nativeElement, `language-${lang}`)
+  }
+
+  ngAfterViewInit() {
+    Prism.highlightElement(this.element.nativeElement)
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { NgModule, Type } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { SourceViewer } from './source-viewer.component'
+import { HightLight } from './highlight.directive'
 
 const TYPES: Type<any>[] = [
   SourceViewer,
+  HightLight,
 ]
 
 @NgModule({
