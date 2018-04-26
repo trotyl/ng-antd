f46a8849ff424bc41b82038e82d281c08264fa99
Trotyl <trotyl@qq.com>
2018-04-26 23:00:51 +0800

    feat(dropdown): support basic hover trigger

--- src/lib/dropdown/dropdown.module.ts
+++ src/lib/dropdown/dropdown.module.ts
@@ -1,14 +1,17 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { OverlayModule } from '@angular/cdk/overlay'
 import { Dropdown } from './dropdown'
+import { Overlay } from './overlay'
 
 const TYPES = [
   Dropdown,
+  Overlay,
 ]
 
 @NgModule({
   declarations: [ TYPES ],
-  imports: [ CommonModule ],
+  imports: [ CommonModule, OverlayModule ],
   exports: [ TYPES ],
 })
 export class DropdownModule { }
---
+++ src/lib/dropdown/overlay.ts
@@ -0,0 +1,6 @@
+import { Directive } from '@angular/core'
+
+@Directive({
+  selector: '[antOverlay]'
+})
+export class Overlay { }
