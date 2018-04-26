a7956bce223f18560df2bec0e79702dcf1fa08e1
Trotyl <trotyl@qq.com>
2018-04-26 23:18:48 +0800

    docs(button): simplify size demo with radio group

--- src/app/button/button-demo-size.ts
+++ src/app/button/button-demo-size.ts
@@ -7,8 +7,4 @@ import { Component } from '@angular/core'
 })
 export class ButtonDemoSize {
   size = 'large'
-
-  handleSizeChange(event: Event) {
-    this.size = (event.target as HTMLInputElement).value
-  }
 }
