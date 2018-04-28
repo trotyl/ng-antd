9e3f64d944380f95ad2533f8761300e36c6cd141
Trotyl <trotyl@qq.com>
2018-04-29 01:41:03 +0800

    feat(slider): support basic slider

--- src/lib/core/lang.ts
+++ src/lib/core/lang.ts
@@ -1,3 +1,8 @@
+export type OnChangeFn<T> = (value: T | null) => void
+export type OnTouchedFn = () => void
+
+export function noop(): void { }
+
 const sizeMap = {
   'large': 'lg',
   'small': 'sm',
@@ -8,4 +13,9 @@ export function getSizeToken(value: 'large' | 'small' | null): 'lg' | 'sm' | 'no
     throw new Error(`Invalid size value: ${value}`)
   }
   return (value && sizeMap[value] as 'lg' | 'sm') || 'noop'
+}
+
+export function range(start: number, end: number): number[] {
+  const length = end - start + 1
+  return new Array(length).fill(0).map((_, i) => start + i)
 }
