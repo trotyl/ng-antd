d70102ca72f9bfb591a8f50af9d01f884467953b
Trotyl <trotyl@qq.com>
2018-04-28 14:59:51 +0800

    style: preder async function

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,11 @@
 import { browser, by, element } from 'protractor'
 
 export class AppPage {
-  navigateTo() {
+  async navigateTo() {
     return browser.get('/')
   }
 
-  getParagraphText() {
+  async getParagraphText() {
     return element(by.css('app-root h1')).getText()
   }
 }
