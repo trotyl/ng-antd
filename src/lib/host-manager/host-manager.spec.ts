7d0bbc4ccc2b460bf4dd101ec175455bf0dffada
Trotyl <trotyl@qq.com>
2018-05-01 19:49:21 +0800

    test(host-manager): improve test coverage for host-manager

--- src/lib/host-manager/host-manager.spec.ts
+++ src/lib/host-manager/host-manager.spec.ts
@@ -1,19 +1,22 @@
-import { Component, Self } from '@angular/core'
+import { Component, Injector, Self } from '@angular/core'
 import { TestBed } from '@angular/core/testing'
 import { assertClass, assertStyle } from '../testing/helper'
-import { HostManager } from './host-manager'
+import { HostManager, HostManagerFactory } from './host-manager'
+import { HostManagerModule } from './host-manager.module'
 
 describe('HostManager', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ HostManagerModule ],
       declarations: [
-        HostElementTest,
+        HostManagerTest,
+        HostManagerFactoryTest,
       ],
     }).compileComponents()
   })
 
   it('should set host classes', () => {
-    const fixture = TestBed.createComponent(HostElementTest)
+    const fixture = TestBed.createComponent(HostManagerTest)
     const component = fixture.componentInstance
 
     component.host.staticClasses = [ 'static' ]
@@ -23,24 +26,38 @@ describe('HostManager', () => {
       'bar': false,
     }
 
-    fixture.detectChanges()
+    assertClass(fixture.debugElement, [`static`, `foo`], [`bar`])
+  })
 
-    assertClass(fixture.debugElement, [`static`, `foo`])
+  it('should change host classes', () => {
+    const fixture = TestBed.createComponent(HostManagerTest)
+    const component = fixture.componentInstance
+
+    component.host.classes = {
+      'foo': true,
+      'bar': true,
+      'baz': true,
+    }
+
+    component.host.classes = {
+      'foo': true,
+      'bar': false,
+    }
+
+    assertClass(fixture.debugElement, [`foo`], [`bar`, `baz`])
   })
 
   it('should add host classes', () => {
-    const fixture = TestBed.createComponent(HostElementTest)
+    const fixture = TestBed.createComponent(HostManagerTest)
     const component = fixture.componentInstance
 
     component.host.addClass('baz')
-
-    fixture.detectChanges()
 
     assertClass(fixture.debugElement, [`baz`])
   })
 
   it('should set host styles', () => {
-    const fixture = TestBed.createComponent(HostElementTest)
+    const fixture = TestBed.createComponent(HostManagerTest)
     const component = fixture.componentInstance
 
     component.host.staticStyles = {
@@ -52,20 +69,56 @@ describe('HostManager', () => {
       'padding': '5px',
     }
 
-    fixture.detectChanges()
-
     assertStyle(fixture.debugElement, {
       'height': '20px',
       'margin': '10px',
       'padding': '5px',
     })
   })
+
+  it('should change host styles', () => {
+    const fixture = TestBed.createComponent(HostManagerTest)
+    const component = fixture.componentInstance
+
+    component.host.styles = {
+      'margin': '10px',
+      'padding': '5px',
+      'height': '20px',
+    }
+
+    component.host.styles = {
+      'margin': '20px',
+      'padding': null!,
+    }
+
+    assertStyle(fixture.debugElement, {
+      'margin': '20px',
+    })
+  })
+
+  it('should dynamic create host manager', () => {
+    const fixture = TestBed.createComponent(HostManagerFactoryTest)
+    const component = fixture.componentInstance
+
+    const host = component.hostFactory.create(component.injector)
+    host.addClass('foo')
+
+    assertClass(fixture.debugElement, [`foo`])
+  })
+
 })
 
 @Component({
   template: '',
   providers: [ HostManager ],
 })
-class HostElementTest {
+class HostManagerTest {
   constructor(@Self() public host: HostManager) { }
+}
+
+@Component({
+  template: '',
+})
+class HostManagerFactoryTest {
+  constructor(public injector: Injector, public hostFactory: HostManagerFactory) { }
 }
