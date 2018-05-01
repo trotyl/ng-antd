734efdedc15dbe03bc575ac4e7dbb001149533b5
Trotyl <trotyl@qq.com>
2018-05-01 15:29:27 +0800

    refactor(util): separate size helper

--- src/lib/util/lang.ts
+++ src/lib/util/lang.ts
@@ -3,18 +3,6 @@ export type OnTouchedFn = () => void
 
 export function noop(): void { }
 
-const sizeMap = {
-  'large': 'lg',
-  'small': 'sm',
-}
-
-export function getSizeToken(value: 'large' | 'small' | null): 'lg' | 'sm' | 'noop' {
-  if (value && !sizeMap.hasOwnProperty(value)) {
-    throw new Error(`Invalid size value: ${value}`)
-  }
-  return (value && sizeMap[value] as 'lg' | 'sm') || 'noop'
-}
-
 export function range(start: number, end: number): number[] {
   const length = end - start + 1
   return new Array(length).fill(0).map((_, i) => start + i)
