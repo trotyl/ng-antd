2147c3aeec21e8a3f3a77e2d9a1633135aad6a33
Trotyl <trotyl@qq.com>
2018-05-14 16:10:53 +0800

    refactor(button): make button-group component reactive

--- src/lib/button/button-group.ts
+++ src/lib/button/button-group.ts
@@ -1,35 +1,49 @@
-import { Directive, Inject, Input, OnChanges, OnInit, Optional, Self, SimpleChanges } from '@angular/core'
+import { Directive, Inject, Input, OnChanges, OnDestroy, Optional, Self, SimpleChanges } from '@angular/core'
+/* tslint:disable-next-line:no-unused-variable */
+import { Observable, Subject } from 'rxjs'
+import { map, takeUntil } from 'rxjs/operators'
 import { Governor } from '../extension/governor'
-import { getSizeToken } from '../util/size'
+import { extractInputs, updateClass } from '../util/reactive'
 import { BUTTON_GROUP_PREFIX } from './token'
+import { toButtonSize } from './util'
 
 @Directive({
   selector: 'ant-btn-group, [antBtnGroup]',
 })
-export class ButtonGroup implements OnChanges, OnInit {
-  @Input() size: 'large' | 'small' | null = null
+export class ButtonGroup implements OnChanges, OnDestroy {
+  @Input() antBtnGroup: 'large' | 'small' | '' | null
+  @Input() size: 'large' | 'small' | null
 
-  @Input()
-  set antBtnGroup(value: 'large' | 'small' | '' | null) {
-    if (value !== '') { this.size = value }
-  }
+  onChanges$ = new Subject<SimpleChanges>()
+  onDestroy$ = new Subject<void>()
+
+  input$ = this.onChanges$.pipe(
+    extractInputs({
+      antBtnGroup: null as string | null,
+      size: null as string | null,
+    }),
+  )
 
   constructor(
-    @Inject(BUTTON_GROUP_PREFIX) private prefix: string,
-    @Optional() @Self() private governor: Governor,
-  ) { }
+    @Inject(BUTTON_GROUP_PREFIX) prefix: string,
+    @Optional() @Self() governor: Governor,
+  ) {
+    governor.configureStaticClasses([ prefix ])
 
-  ngOnChanges(changes: SimpleChanges): void {
-    this.updateHostClasses()
+    const className$ = this.input$.pipe(
+      map(({ antBtnGroup, size }) => ({
+        [`${prefix}-${toButtonSize(size || antBtnGroup)}`]: !!(size || antBtnGroup),
+      })),
+      updateClass(governor),
+    )
+
+    const status$ = className$.pipe(
+      takeUntil(this.onDestroy$),
+    )
+
+    status$.subscribe()
   }
 
-  ngOnInit(): void {
-    this.governor.configureStaticClasses([ this.prefix ])
-  }
-
-  private updateHostClasses(): void {
-    this.governor.configureClasses({
-      [`${this.prefix}-${getSizeToken(this.size, 'antBtnGroup')}`]: !!this.size,
-    })
-  }
+  ngOnChanges(changes: SimpleChanges): void { this.onChanges$.next(changes) }
+  ngOnDestroy(): void { this.onDestroy$.next() }
 }
