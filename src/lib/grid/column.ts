206db42b25208c1a03680b80e6b776e5a6610f65
Trotyl <trotyl@qq.com>
2018-05-15 10:53:59 +0800

    refactor(grid): make row and column reactive

--- src/lib/grid/column.ts
+++ src/lib/grid/column.ts
@@ -1,9 +1,11 @@
 import { Directive, Host, Inject, Input, OnChanges, OnDestroy, OnInit, Optional, Self, SimpleChanges } from '@angular/core'
-import { merge, Subject, Subscription } from 'rxjs'
-import { tap } from 'rxjs/operators'
+/* tslint:disable-next-line:no-unused-variable */
+import { combineLatest, merge, Observable, Subject } from 'rxjs'
+import { map, switchMap, takeUntil, tap } from 'rxjs/operators'
 import { Governor } from '../extension/governor'
-import { Responsive, ResponsiveOption as Rsp } from '../responsive/responsive'
-import { assert, empty } from '../util/debug'
+import { Responsive } from '../responsive/responsive'
+import { assert } from '../util/debug'
+import { extractInputs, updateClass, updateStyle } from '../util/reactive'
 import { Row } from './row'
 import { COLUMN_PREFIX } from './token'
 
@@ -11,128 +13,135 @@ import { COLUMN_PREFIX } from './token'
   selector: 'ant-col, [antCol]',
 })
 export class Column implements OnChanges, OnDestroy, OnInit {
-  @Input() span: number = -1
-  @Input() offset: number = 0
-  @Input() order: number = 0
-  @Input() pull: number = 0
-  @Input() push: number = 0
+  @Input() antCol: number | '' | null
+  @Input() span: number | null
+  @Input() offset: number | null
+  @Input() order: number | null
+  @Input() pull: number | null
+  @Input() push: number | null
 
-  @Input()
-  set antCol(value: number | '') {
-    if (value !== '') { this.span = value }
-  }
+  @Input('span.xs') spanXs: number | null
+  @Input('span.sm') spanSm: number | null
+  @Input('span.md') spanMd: number | null
+  @Input('span.lg') spanLg: number | null
+  @Input('span.xl') spanXl: number | null
+  @Input('span.xxl') spanXxl: number | null
 
-  @Input('span.xs') set spanXs(value: number) { this.rSpan.xs = value }
-  @Input('span.sm') set spanSm(value: number) { this.rSpan.sm = value }
-  @Input('span.md') set spanMd(value: number) { this.rSpan.md = value }
-  @Input('span.lg') set spanLg(value: number) { this.rSpan.lg = value }
-  @Input('span.xl') set spanXl(value: number) { this.rSpan.xl = value }
-  @Input('span.xxl') set spanXxl(value: number) { this.rSpan.xxl = value }
+  @Input('offset.xs') offsetXs: number | null
+  @Input('offset.sm') offsetSm: number | null
+  @Input('offset.md') offsetMd: number | null
+  @Input('offset.lg') offsetLg: number | null
+  @Input('offset.xl') offsetXl: number | null
+  @Input('offset.xxl') offsetXxl: number | null
 
-  @Input('offset.xs') set offsetXs(value: number) { this.rOffset.xs = value }
-  @Input('offset.sm') set offsetSm(value: number) { this.rOffset.sm = value }
-  @Input('offset.md') set offsetMd(value: number) { this.rOffset.md = value }
-  @Input('offset.lg') set offsetLg(value: number) { this.rOffset.lg = value }
-  @Input('offset.xl') set offsetXl(value: number) { this.rOffset.xl = value }
-  @Input('offset.xxl') set offsetXxl(value: number) { this.rOffset.xxl = value }
+  @Input('order.xs') orderXs: number | null
+  @Input('order.sm') orderSm: number | null
+  @Input('order.md') orderMd: number | null
+  @Input('order.lg') orderLg: number | null
+  @Input('order.xl') orderXl: number | null
+  @Input('order.xxl') orderXxl: number | null
 
-  @Input('order.xs') set orderXs(value: number) { this.rOrder.xs = value }
-  @Input('order.sm') set orderSm(value: number) { this.rOrder.sm = value }
-  @Input('order.md') set orderMd(value: number) { this.rOrder.md = value }
-  @Input('order.lg') set orderLg(value: number) { this.rOrder.lg = value }
-  @Input('order.xl') set orderXl(value: number) { this.rOrder.xl = value }
-  @Input('order.xxl') set orderXxl(value: number) { this.rOrder.xxl = value }
+  @Input('pull.xs') pullXs: number | null
+  @Input('pull.sm') pullSm: number | null
+  @Input('pull.md') pullMd: number | null
+  @Input('pull.lg') pullLg: number | null
+  @Input('pull.xl') pullXl: number | null
+  @Input('pull.xxl') pullXxl: number | null
 
-  @Input('pull.xs') set pullXs(value: number) { this.rPull.xs = value }
-  @Input('pull.sm') set pullSm(value: number) { this.rPull.sm = value }
-  @Input('pull.md') set pullMd(value: number) { this.rPull.md = value }
-  @Input('pull.lg') set pullLg(value: number) { this.rPull.lg = value }
-  @Input('pull.xl') set pullXl(value: number) { this.rPull.xl = value }
-  @Input('pull.xxl') set pullXxl(value: number) { this.rPull.xxl = value }
+  @Input('push.xs') pushXs: number | null
+  @Input('push.sm') pushSm: number | null
+  @Input('push.md') pushMd: number | null
+  @Input('push.lg') pushLg: number | null
+  @Input('push.xl') pushXl: number | null
+  @Input('push.xxl') pushXxl: number | null
 
-  @Input('push.xs') set pushXs(value: number) { this.rPush.xs = value }
-  @Input('push.sm') set pushSm(value: number) { this.rPush.sm = value }
-  @Input('push.md') set pushMd(value: number) { this.rPush.md = value }
-  @Input('push.lg') set pushLg(value: number) { this.rPush.lg = value }
-  @Input('push.xl') set pushXl(value: number) { this.rPush.xl = value }
-  @Input('push.xxl') set pushXxl(value: number) { this.rPush.xxl = value }
+  onChanges$ = new Subject<SimpleChanges>()
+  onInit$ = new Subject<void>()
+  onDestroy$ = new Subject<void>()
 
-  rSpan: Rsp<number> = {}
-  rOffset: Rsp<number> = {}
-  rOrder: Rsp<number> = {}
-  rPull: Rsp<number> = {}
-  rPush: Rsp<number> = {}
-
-  fSpan: number = 0
-  fOffset: number = 0
-  fOrder: number = 0
-  fPull: number = 0
-  fPush: number = 0
-
-  private changes$ = new Subject<void>()
-
-  private status$$: Subscription
-  private rowStatus$$: Subscription
+  input$ = this.onChanges$.pipe(
+    extractInputs({
+      antCol: null as number | null,
+      span: null as number | null, spanXs: null as number | null, spanSm: null as number | null, spanMd: null as number | null, spanLg: null as number | null, spanXl: null as number | null, spanXxl: null as number | null,
+      offset: null as number | null, offsetXs: null as number | null, offsetSm: null as number | null, offsetMd: null as number | null, offsetLg: null as number | null, offsetXl: null as number | null, offsetXxl: null as number | null,
+      order: null as number | null, orderXs: null as number | null, orderSm: null as number | null, orderMd: null as number | null, orderLg: null as number | null, orderXl: null as number | null, orderXxl: null as number | null,
+      pull: null as number | null, pullXs: null as number | null, pullSm: null as number | null, pullMd: null as number | null, pullLg: null as number | null, pullXl: null as number | null, pullXxl: null as number | null,
+      push: null as number | null, pushXs: null as number | null, pushSm: null as number | null, pushMd: null as number | null, pushLg: null as number | null, pushXl: null as number | null, pushXxl: null as number | null,
+    }),
+    tap(inputs => inputs.span = inputs.span != null ? inputs.span : inputs.antCol),
+  )
 
   constructor(
-    @Inject(COLUMN_PREFIX) private prefix: string,
-    private rsp: Responsive,
-    @Optional() @Self() private governor: Governor,
-    @Optional() @Host() private row: Row,
+    @Inject(COLUMN_PREFIX) prefix: string,
+    rsp: Responsive,
+    @Optional() @Self() governor: Governor,
+    @Optional() @Host() row: Row,
   ) {
-    /*@__PURE__*/assert(`antCol: missing 'antRow' in scope`, !row)
+    /*@__PURE__*/checkDeps(row)
+
+    governor.configureStaticClasses([ prefix ])
+
+    const rSpan$ = this.input$.pipe(map(({ span, spanXs: xs, spanSm: sm, spanMd: md, spanLg: lg, spanXl: xl, spanXxl: xxl }) => ({ xs, sm, md, lg, xl, xxl, default: span })))
+    const rOffset$ = this.input$.pipe(map(({ offset, offsetXs: xs, offsetSm: sm, offsetMd: md, offsetLg: lg, offsetXl: xl, offsetXxl: xxl }) => ({ xs, sm, md, lg, xl, xxl, default: offset })))
+    const rOrder$ = this.input$.pipe(map(({ order, orderXs: xs, orderSm: sm, orderMd: md, orderLg: lg, orderXl: xl, orderXxl: xxl }) => ({ xs, sm, md, lg, xl, xxl, default: order })))
+    const rPull$ = this.input$.pipe(map(({ pull, pullXs: xs, pullSm: sm, pullMd: md, pullLg: lg, pullXl: xl, pullXxl: xxl }) => ({ xs, sm, md, lg, xl, xxl, default: pull })))
+    const rPush$ = this.input$.pipe(map(({ push, pushXs: xs, pushSm: sm, pushMd: md, pushLg: lg, pushXl: xl, pushXxl: xxl }) => ({ xs, sm, md, lg, xl, xxl, default: push })))
+
+    const responsive$ = combineLatest(
+      rSpan$.pipe(switchMap(rSpan => rsp.resolve(rSpan))),
+      rOffset$.pipe(switchMap(rOffset => rsp.resolve(rOffset))),
+      rOrder$.pipe(switchMap(rOrder => rsp.resolve(rOrder))),
+      rPull$.pipe(switchMap(rPull => rsp.resolve(rPull))),
+      rPush$.pipe(switchMap(rPush => rsp.resolve(rPush))),
+    ).pipe(
+      map(([span, offset, order, pull, push]) => ({ span, offset, order, pull, push })),
+    )
+
+    const className$ = responsive$.pipe(
+      map(({ span, offset, order, pull, push }) => ({
+        [`${prefix}-${span}`]: true,
+        [`${prefix}-offset-${offset}`]: offset != null,
+        [`${prefix}-pull-${pull}`]: pull != null,
+        [`${prefix}-push-${push}`]: push != null,
+        [`${prefix}-order-${order}`]: order != null,
+      })),
+      updateClass(governor),
+    )
+
+    const style$ = row.gutter$.pipe(
+      map((gutter) => {
+        const padding = gutter / 2
+        if (padding > 0) {
+          return {
+            'padding-left': `${padding}px`,
+            'padding-right': `${padding}px`,
+          }
+        } else {
+          return {} as { [name: string]: string }
+        }
+      }),
+      updateStyle(governor),
+    )
+
+    const status$ = merge(
+      className$,
+      style$,
+    ).pipe(
+      takeUntil(this.onDestroy$),
+    )
+
+    status$.subscribe()
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    this.changes$.next()
-  }
+  ngOnChanges(changes: SimpleChanges): void { /*@__PURE__*/checkInputs(this); this.onChanges$.next(changes) }
+  ngOnInit(): void { /*@__PURE__*/checkInputs(this); this.onInit$.next() }
+  ngOnDestroy(): void { this.onDestroy$.next() }
+}
 
-  ngOnInit(): void {
-    /*@__PURE__*/assert(`antCol: missing 'span' input`, this.span < 0, /*@__PURE__*/empty(this.rSpan))
+function checkDeps(row: Row | null): void {
+  assert(`antCol: missing 'antRow' in scope`, !row)
+}
 
-    this.governor.configureStaticClasses([ this.prefix ])
-
-    this.rowStatus$$ = this.row.status$.subscribe(() => this.updateHostStyles())
-
-    const span$ = this.rsp.resolve(this.rSpan, () => this.span, this.changes$)
-    const offset$ = this.rsp.resolve(this.rOffset, () => this.offset, this.changes$)
-    const order$ = this.rsp.resolve(this.rOrder, () => this.order, this.changes$)
-    const pull$ = this.rsp.resolve(this.rPull, () => this.pull, this.changes$)
-    const push$ = this.rsp.resolve(this.rPush, () => this.push, this.changes$)
-
-    this.status$$ = merge(
-      span$.pipe(tap(x => this.fSpan = x)),
-      offset$.pipe(tap(x => this.fOffset = x)),
-      order$.pipe(tap(x => this.fOrder = x)),
-      pull$.pipe(tap(x => this.fPull = x)),
-      push$.pipe(tap(x => this.fPush = x)),
-    ).subscribe(() => this.updateHostClasses())
-  }
-
-  ngOnDestroy(): void {
-    if (this.status$$) this.status$$.unsubscribe()
-    if (this.rowStatus$$) this.rowStatus$$.unsubscribe()
-  }
-
-  private updateHostClasses(): void {
-    this.governor.configureClasses({
-      [`${this.prefix}-${this.fSpan}`]: true,
-      [`${this.prefix}-offset-${this.fOffset}`]: this.fOffset > 0,
-      [`${this.prefix}-pull-${this.fPull}`]: this.fPull > 0,
-      [`${this.prefix}-push-${this.fPush}`]: this.fPush > 0,
-      [`${this.prefix}-order-${this.fOrder}`]: this.fOrder > 0,
-    })
-  }
-
-  private updateHostStyles(): void {
-    const padding = this.row.fGutter / 2
-    if (padding !== 0) {
-      this.governor.configureStyles({
-        'padding-left': `${padding}px`,
-        'padding-right': `${padding}px`,
-      })
-    } else {
-      this.governor.configureStyles({})
-    }
-  }
+function checkInputs(ctx: Column): void {
+  assert(`antCol: missing 'span' input`, ctx.antCol == null, ctx.span == null, ctx.spanXs == null, ctx.spanSm == null, ctx.spanMd == null, ctx.spanLg == null, ctx.spanXl == null, ctx.spanXxl == null)
 }
--- src/lib/grid/row.ts
+++ src/lib/grid/row.ts
@@ -1,82 +1,89 @@
 import { Directive, Inject, Input, OnChanges, OnDestroy, OnInit, Optional, Self, SimpleChanges } from '@angular/core'
-import { Observable, Subject, Subscription } from 'rxjs'
-import { map, tap } from 'rxjs/operators'
+import { merge, Observable, Subject } from 'rxjs'
+import { map, switchMap, takeUntil, tap } from 'rxjs/operators'
 import { Governor } from '../extension/governor'
-import { Responsive, ResponsiveOption as Rsp } from '../responsive/responsive'
+import { Responsive } from '../responsive/responsive'
+import { extractInputs, updateClass, updateStyle } from '../util/reactive'
 import { ROW_PREFIX } from './token'
 
 @Directive({
   selector: 'ant-row, [antRow]',
 })
 export class Row implements OnChanges, OnDestroy, OnInit {
-  @Input() align: 'top' | 'middle' | 'bottom' | null = null
-  @Input() gutter: number = 0
-  @Input() justify: 'start' | 'end' | 'center' | 'space-around' | 'space-between' | null = null
-  @Input() type: 'flex' | null = null
+  @Input() antRow: 'flex' | '' | null
+  @Input() align: 'top' | 'middle' | 'bottom' | null
+  @Input() gutter: number
+  @Input() justify: 'start' | 'end' | 'center' | 'space-around' | 'space-between' | null
+  @Input() type: 'flex' | null
 
-  @Input()
-  set antRow(value: 'flex' | '' | null) {
-    if (value !== '') { this.type = value }
-  }
+  @Input('gutter.xs') gutterXs: number
+  @Input('gutter.sm') gutterSm: number
+  @Input('gutter.md') gutterMd: number
+  @Input('gutter.lg') gutterLg: number
+  @Input('gutter.xl') gutterXl: number
+  @Input('gutter.xxl') gutterXxl: number
 
-  @Input('gutter.xs') set gutterXs(value: number) { this.rGutter.xs = value }
-  @Input('gutter.sm') set gutterSm(value: number) { this.rGutter.sm = value }
-  @Input('gutter.md') set gutterMd(value: number) { this.rGutter.md = value }
-  @Input('gutter.lg') set gutterLg(value: number) { this.rGutter.lg = value }
-  @Input('gutter.xl') set gutterXl(value: number) { this.rGutter.xl = value }
-  @Input('gutter.xxl') set gutterXxl(value: number) { this.rGutter.xxl = value }
+  onChanges$ = new Subject<SimpleChanges>()
+  onInit$ = new Subject<void>()
+  onDestroy$ = new Subject<void>()
 
-  status$: Observable<void>
-  status$$: Subscription
+  input$ = this.onChanges$.pipe(
+    extractInputs({
+      antRow: null as string | null,
+      type: null as string | null, align: null as string | null, justify: null as string | null,
+      gutter: 0, gutterXs: null as number | null, gutterSm: null as number | null, gutterMd: null as number | null, gutterLg: null as number | null, gutterXl: null as number | null, gutterXxl: null as number | null,
+    }),
+    tap(inputs => inputs.type = inputs.type != null ? inputs.type : inputs.antRow),
+  )
 
-  rGutter: Rsp<number> = {}
-  fGutter: number = 0
-
-  private changes$ = new Subject<void>()
+  gutter$: Observable<number>
 
   constructor(
-    @Inject(ROW_PREFIX) private prefix: string,
-    private rsp: Responsive,
-    @Optional() @Self() private governor: Governor,
-  ) { }
+    @Inject(ROW_PREFIX) prefix: string,
+    rsp: Responsive,
+    @Optional() @Self() governor: Governor,
+  ) {
+    this.gutter$ = this.input$.pipe(
+      map(({ gutter, gutterXs: xs, gutterSm: sm, gutterMd: md, gutterLg: lg, gutterXl: xl, gutterXxl: xxl }) => ({ xs, sm, md, lg, xl, xxl, default: gutter })),
+      switchMap(rGutter => rsp.resolve(rGutter)),
+    )
 
-  ngOnChanges(changes: SimpleChanges): void {
-    this.changes$.next()
-    this.updateHostClasses()
+    const className$ = this.input$.pipe(
+      map(({ type, justify, align }) => ({
+        [`${prefix}`]: type !== 'flex',
+        [`${prefix}-flex`]: type === 'flex',
+        [`${prefix}-flex-${justify}`]: type === 'flex' && justify != null,
+        [`${prefix}-flex-${align}`]: type === 'flex' && align != null,
+      })),
+      updateClass(governor),
+    )
+
+    const style$ = this.gutter$.pipe(
+      map((gutter) => {
+        const margin = gutter / -2
+        if (margin !== 0) {
+          return {
+            'margin-left': `${margin}px`,
+            'margin-right': `${margin}px`,
+          }
+        } else {
+          return {} as { [name: string]: string }
+        }
+      }),
+      updateStyle(governor),
+    )
+
+    const status$ = merge(
+      className$,
+      style$,
+    ).pipe(
+      takeUntil(this.onDestroy$),
+    )
+
+    status$.subscribe()
   }
 
-  ngOnInit(): void {
-    const gutter$ = this.rsp.resolve(this.rGutter, () => this.gutter, this.changes$)
-    this.status$ = gutter$.pipe(tap(x => this.fGutter = x), map(() => {}))
-    this.status$$ = this.status$.subscribe(() => this.updateHostStyles())
-
-    this.updateHostClasses()
-  }
-
-  ngOnDestroy(): void {
-    /* istanbul ignore else */
-    if (this.status$$) this.status$$.unsubscribe()
-  }
-
-  private updateHostClasses(): void {
-    const isFlex = this.type === 'flex'
-    this.governor.configureClasses({
-      [`${this.prefix}`]: !isFlex,
-      [`${this.prefix}-flex`]: isFlex,
-      [`${this.prefix}-flex-${this.justify}`]: !!this.justify,
-      [`${this.prefix}-flex-${this.align}`]: !!this.align,
-    })
-  }
-
-  private updateHostStyles(): void {
-    const margin = this.fGutter / -2
-    if (margin !== 0) {
-      this.governor.configureStyles({
-        'margin-left': `${margin}px`,
-        'margin-right': `${margin}px`,
-      })
-    } else {
-      this.governor.configureStyles({})
-    }
-  }
+  ngOnChanges(changes: SimpleChanges): void { this.onChanges$.next(changes) }
+  ngOnInit(): void { this.onInit$.next() }
+  ngOnDestroy(): void { this.onDestroy$.next() }
 }
