55d212620ce1cc916c36ccd1f1457820b9f64259
Zejiang Yu <trotyl@qq.com>
2019-10-06 23:35:52 +0800

    refactor(docs-infra): extract layout component to align with antd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core'
-import { Meta } from '@angular/platform-browser'
-
-const title = 'Ant Design - A UI Design Language and React UI library'
-const description = 'An enterprise-class UI design language and React UI library with a set of high-quality React components, one of best React UI library for enterprises'
 
 @Component({
   selector: 'app-root',
@@ -12,16 +8,4 @@ const description = 'An enterprise-class UI design language and React UI library
 })
 export class AppComponent {
   @HostBinding('id') id = 'react-content'
-
-  constructor(
-    private meta: Meta,
-  ) {}
-
-  ngOnInit(): void {
-    // TODO: extract to template-based definition
-    this.meta.addTag({ name: 'description', content: description })
-    this.meta.addTag({ property: 'og:title', content: title })
-    this.meta.addTag({ property: 'og:type', content: 'website' })
-    this.meta.addTag({ property: 'og:image', content: 'https://gw.alipayobjects.com/zos/rmsportal/rlpTLlbMzTNYuZGGCVYM.png' })
-  }
 }
